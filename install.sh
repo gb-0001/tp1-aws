162525f23fb7d2853ea6d14ad835c91d84ba0987
gb-0001 <gb-0001@outlook.fr>
2021-06-17 11:46:02 +0200

    MAJ install.sh Mariadb

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 yum install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm -y 1>>/tmp/trace.log 2>&1
 amazon-linux-extras install epel -y 1>>/tmp/trace.log 2>&1
 yum install httpd openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip -y 1>>/tmp/trace.log 2>&1
-yum install mysql-community-server -y 1>>/tmp/trace.log 2>&1
-systemctl enable --now mysqld 1>>/tmp/trace.log 2>&1
-MYSQLPWD=$(grep 'temporary password' /var/log/mysqld.log | awk -F"root@localhost: " '{print $2}' | awk 1 ORS='' 1>>/tmp/trace.log 2>&1)
-echo $MYSQLPWD
-mysqladmin -u root -p$MYSQLPWD create blog 1>>/tmp/trace.log 2>&1
+yum install mariadb-server -y 1>>/tmp/trace.log 2>&1
+#yum install mysql-community-server -y 1>>/tmp/trace.log 2>&1
+systemctl start mariadb.service 1>>/tmp/trace.log 2>&1
+#MYSQLPWD=$(grep 'temporary password' /var/log/mysqld.log | awk -F"root@localhost: " '{print $2}' | awk 1 ORS='' 1>>/tmp/trace.log 2>&1)
+#echo $MYSQLPWD
+mysqladmin -u root create blog 1>>/tmp/trace.log 2>&1
 (echo
 echo Y
 echo password
