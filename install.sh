c465742140fe5cba5e4a066d537e2d8153753541
gb-0001 <gb-0001@outlook.fr>
2021-06-17 10:43:32 +0200

    MAJ install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-yum install httpd openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip
-yum install mysql-server
-service mysqld start
-mysqladmin -u root create blog
+yum install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm -y 1>>/tmp/trace.log 2>&1
+amazon-linux-extras install epel -y 1>>/tmp/trace.log 2>&1
+yum install httpd openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip -y 1>>/tmp/trace.log 2>&1
+yum install mysql-community-server -y 1>>/tmp/trace.log 2>&1
+systemctl enable --now mysqld 1>>/tmp/trace.log 2>&1
+grep 'temporary password' /var/log/mysqld.log 1>>/tmp/trace.log 2>&1
+mysqladmin -u root create blog 1>>/tmp/trace.log 2>&1
 (echo
 echo Y
 echo password
@@ -11,8 +14,8 @@ echo Y
 echo Y
 echo Y
 echo Y
-)| mysql_secure_installation
+)| mysql_secure_installation 1>>/tmp/trace.log 2>&1
 
-cd /var/www/html
-wget http://wordpress.org/latest.tar.gz
-tar -xzvf latest-fr_FR.tar.gz .
+cd /var/www/html 1>>/tmp/trace.log 2>&1
+wget http://wordpress.org/latest.tar.gz 1>>/tmp/trace.log 2>&1
+tar -xzvf latest.tar.gz . 1>>/tmp/trace.log 2>&1
--- tp1.sh
+++ tp1.sh
@@ -28,73 +28,73 @@ INSTANCETYP="t2.micro"
 IMGID="ami-00c08ad1a6ca8ca7c"
 
 #creation vpc
-echo "=========START creation vpc=======" >> trace.log
+echo "=========START creation vpc=======" 1>>trace.log 2>&1
 AWSVPC=$($AWSBIN ec2 create-vpc --cidr-block "$VPCCIDRBLK" --instance-tenancy default --endpoint $ZONE)
-echo $AWSVPC >> trace.log
+echo $AWSVPC 1>>trace.log 2>&1
 IDVPC=$(echo -e "$AWSVPC" |  jq '.Vpc.VpcId' | tr -d '"')
-echo $IDVPC >> trace.log
-echo "=========END creation vpc=======" >> trace.log
-echo "" >> trace.log
+echo $IDVPC 1>>trace.log 2>&1
+echo "=========END creation vpc=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #public
-echo "=========START creation subnet public=======" >> trace.log
+echo "=========START creation subnet public=======" 1>>trace.log 2>&1
 AWSVPCSUB1=$($AWSBIN ec2 create-subnet --vpc-id $IDVPC --cidr-block "$PRIVSUBNET")
 RETAWSVPCSUB1ID=$(echo -e "$AWSVPCSUB1" |  jq '.Subnet.SubnetId' | tr -d '"')
-echo "=========END creation subnet public=======" >> trace.log
-echo "" >> trace.log
+echo "=========END creation subnet public=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #prive
-echo "=========START creation subnet prive=======" >> trace.log
+echo "=========START creation subnet prive=======" 1>>trace.log 2>&1
 AWSVPCSUB2=$($AWSBIN ec2 create-subnet --vpc-id $IDVPC --cidr-block "$PUBSUBNET")
 RETAWSVPCSUB2ID=$(echo -e "$AWSVPCSUB2" |  jq '.Subnet.SubnetId' | tr -d '"')
-echo "=========END creation subnet prive=======" >> trace.log
-echo "" >> trace.log
+echo "=========END creation subnet prive=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #creation de la GW internet
-echo "=========START creation GW internet=======" >> trace.log
+echo "=========START creation GW internet=======" 1>>trace.log 2>&1
 RETIGW=$($AWSBIN ec2 create-internet-gateway )
-echo $RETIGW >> trace.log
+echo $RETIGW 1>>trace.log 2>&1
 RETIGWID=$(echo -e "$RETIGW" |  jq '.InternetGateway.InternetGatewayId' | tr -d '"')
-echo $RETIGWID >> trace.log
-echo "=========END creation GW internet=======" >> trace.log
-echo "" >> trace.log
+echo $RETIGWID 1>>trace.log 2>&1
+echo "=========END creation GW internet=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #attache GW to VPC
-echo "=========START attache GW to VPC=======" >> trace.log
+echo "=========START attache GW to VPC=======" 1>>trace.log 2>&1
 RETGWTOVPCLNK=$($AWSBIN ec2 attach-internet-gateway --internet-gateway-id "$RETIGWID" --vpc-id "$IDVPC")
-echo "=========END attache GW to VPC=======" >> trace.log
-echo "" >> trace.log
+echo "=========END attache GW to VPC=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 
 #Creation d'une table de route
-echo "=========START Creation d'une table de route=======" >> trace.log
+echo "=========START Creation d'une table de route=======" 1>>trace.log 2>&1
 RETROUTETAB=$($AWSBIN ec2 create-route-table --vpc-id "$IDVPC")
-echo $RETROUTETAB >> trace.log
+echo $RETROUTETAB 1>>trace.log 2>&1
 RETROUTETABID=$(echo -e "$RETROUTETAB" |  jq '.RouteTable.RouteTableId' | tr -d '"')
-echo $RETROUTETABID >> trace.log
-echo "=========END Creation d'une table de route=======" >> trace.log
-echo "" >> trace.log
+echo $RETROUTETABID 1>>trace.log 2>&1
+echo "=========END Creation d'une table de route=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #Creation d'une route vers la passerelle internet
-echo "=========START Creation d'une route vers la passerelle internet=======" >> trace.log
+echo "=========START Creation d'une route vers la passerelle internet=======" 1>>trace.log 2>&1
 RETROUTETOGWI=$($AWSBIN ec2 create-route --route-table-id "$RETROUTETABID" --destination-cidr-block "$CIDRBLOCK" --gateway-id "$RETIGWID")
-echo $RETROUTETOGWI >> trace.log
-echo "=========END d'une route vers la passerelle internet=======" >> trace.log
-echo "" >> trace.log
+echo $RETROUTETOGWI 1>>trace.log 2>&1
+echo "=========END d'une route vers la passerelle internet=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #Association du sous-réseau public à la table de route
-echo "=========START Association du sous-réseau public à la table de route=======" >> trace.log
+echo "=========START Association du sous-réseau public à la table de route=======" 1>>trace.log 2>&1
 RETSUBPUBTOROUTE=$($AWSBIN ec2 associate-route-table --subnet-id "$RETAWSVPCSUB1ID" --route-table-id "$RETROUTETABID")
-echo $RETROUTETOGWI >> trace.log
-echo "=========END  Association du sous-réseau public à la table de route=======" >> trace.log
-echo "" >> trace.log
+echo $RETROUTETOGWI 1>>trace.log 2>&1
+echo "=========END  Association du sous-réseau public à la table de route=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #enable public ip on subnet
-echo "=========START enable public ip on subnet=======" >> trace.log
+echo "=========START enable public ip on subnet=======" 1>>trace.log 2>&1
 RETPUBIP=$($AWSBIN ec2 modify-subnet-attribute --subnet-id "$RETAWSVPCSUB1ID" --map-public-ip-on-launch)
-echo $RETPUBIP >> trace.log
-echo "=========END  enable public ip on subnet=======" >> trace.log
-echo "" >> trace.log
+echo $RETPUBIP 1>>trace.log 2>&1
+echo "=========END  enable public ip on subnet=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #add DNS
 #RETDNS=$(aw$AWSBINs ec2 modify-vpc-attribute --vpc-id "$IDVPC" --enable-dns-hostnames "{\"Value\":true}")
@@ -106,36 +106,38 @@ echo "" >> trace.log
 
 
 #creation groupe de secu
-echo "=========START creation groupe de secu=======" >> trace.log
+echo "=========START creation groupe de secu=======" 1>>trace.log 2>&1
 AWSGRPSEC=$($AWSBIN ec2 create-security-group --group-name "$SECGRPNAME" --description "GB TP1 GRP SECU" --vpc-id "$echo $IDVPC")
-echo $AWSGRPSEC >> trace.log
+echo $AWSGRPSEC 1>>trace.log 2>&1
 IDGRP=$(echo -e "$AWSGRPSEC" |  jq '.GroupId' | tr -d '"')
-echo $IDGRP  >> trace.log
-echo "=========END  creation groupe de secu=======" >> trace.log
-echo "" >> trace.log
+echo $IDGRP  1>>trace.log 2>&1
+echo "=========END  creation groupe de secu=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #activation port 22
-echo "=========START activation port 22=======" >> trace.log
+echo "=========START activation port 22=======" 1>>trace.log 2>&1
 RETSSH=$($AWSBIN ec2 authorize-security-group-ingress --group-id "$IDGRP" --protocol tcp --port 22 --cidr "$CIDRBLOCK")
-echo $RETSSH >> trace.log
-echo "=========END  activation port 22=======" >> trace.log
-echo "" >> trace.log
+echo $RETSSH 1>>trace.log 2>&1
+echo "=========END  activation port 22=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #activation port 80
-echo "=========START activation port 80=======" >> trace.log
+echo "=========START activation port 80=======" 1>>trace.log 2>&1
 RETHTTP=$($AWSBIN ec2 authorize-security-group-ingress --group-id "$IDGRP" --protocol tcp --port 80 --cidr "$CIDRBLOCK")
-echo $RETHTTP >> trace.log
-echo "=========END  activation port 80=======" >> trace.log
-echo "" >> trace.log
+echo $RETHTTP 1>>trace.log 2>&1
+echo "=========END  activation port 80=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
 
 #creation NAT GW
 #extraire info NAt GW avec le query
 #aws ec2 describe-nat-gateways --query 'NatGateways[*].NatGatewayAddresses[*].PublicIp'
 
 #creation de l'instance
-echo "=========START creation de l'instance=======" >> trace.log
+echo "=========START creation de l'instance=======" 1>>trace.log 2>&1
 RETEC2CREATE=$($AWSBIN ec2 run-instances --image-id $IMGID --count 1 --instance-type $INSTANCETYP --key-name $SSHKEYUSED --security-group-ids $IDGRP --subnet-id $RETAWSVPCSUB1ID --user-data file://install.sh)
 #RETEC2CREATE=$($AWSBIN ec2 run-instances --image-id ami-00c08ad1a6ca8ca7c --count 1 --instance-type $INSTANCETYP --key-name tp1GB --security-group-ids $IDGRP --subnet-id $RETAWSVPCSUB1ID)
-echo $RETEC2CREATE >> trace.log
-echo "=========END  creation de l'instance=======" >> trace.log
-echo "" >> trace.log
+echo $RETEC2CREATE 1>>trace.log 2>&1
+echo "=========END  creation de l'instance=======" 1>>trace.log 2>&1
+echo "" 1>>trace.log 2>&1
+
+cat trace.log
